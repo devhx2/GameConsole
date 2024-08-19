6c98f383a01445b00999479aec14d63cabd6c983
Hx2 <81757443+devhx2@users.noreply.github.com>
2024-08-19 17:28:36 +0900

    入力された文字を非表示にする

--- src/GameConsole.c
+++ src/GameConsole.c
@@ -3,15 +3,27 @@
 #include <Windows.h>
 
 /// @brief スクリーンバッファ1
-HANDLE g_screen_buffer1 = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+HANDLE g_screen_buffer1;
 /// @brief スクリーンバッファ2
-HANDLE g_screen_buffer2 = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+HANDLE g_screen_buffer2;
 
 /// @brief 初期化処理
 void Initialize()
 {
     SetConsoleCP(CP_UTF8);
     SetConsoleOutputCP(CP_UTF8);
+
+    g_screen_buffer1 = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+    g_screen_buffer2 = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+
+    {
+        DWORD console_mode;
+        const HANDLE input_handle = GetStdHandle(STD_INPUT_HANDLE);
+        GetConsoleMode(input_handle, &console_mode);
+        // 入力された文字を非表示にする
+        console_mode &= ~ENABLE_ECHO_INPUT;
+        SetConsoleMode(input_handle, console_mode);
+    }
 }
 
 /// @brief 終了処理
