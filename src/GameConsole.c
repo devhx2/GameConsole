624aedb5d15ccf872958d09d59b20456107a58f6
Hx2 <81757443+devhx2@users.noreply.github.com>
2024-08-19 17:09:45 +0900

    スクリーンバッファを追加

--- src/GameConsole.c
+++ src/GameConsole.c
@@ -2,6 +2,11 @@
 
 #include <Windows.h>
 
+/// @brief スクリーンバッファ1
+HANDLE g_screen_buffer1 = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+/// @brief スクリーンバッファ2
+HANDLE g_screen_buffer2 = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+
 /// @brief 初期化処理
 void Initialize()
 {
@@ -12,4 +17,6 @@ void Initialize()
 /// @brief 終了処理
 void Finalize()
 {
+    CloseHandle(g_screen_buffer1);
+    CloseHandle(g_screen_buffer2);
 }
